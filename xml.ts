d36a248abbab08cd230300b4feb454be28b5744c
rinrab <tima@chemodax.net>
2023-06-04 17:32:12 +0300

    Add XML parser

---
+++ xml.ts
@@ -0,0 +1,107 @@
+namespace XML {
+    const enum TokenType {
+        Tag,
+        CloseTag,
+        AttributeName,
+        AttributeValue,
+        Text,
+    }
+
+    interface Token {
+        type: TokenType;
+        value: string;
+    }
+
+    export function parse(text: string): Token[] {
+        const tokens: Token[] = [];
+        let i = 0;
+        let isInTag = false;
+        while (i < text.length) {
+            if (text[i] == '<') {
+                i++;
+                if (text[i] == '/') {
+                    i++;
+                    let tagName = "";
+                    while (i < text.length) {
+                        if (text[i] == '>') {
+                            break;
+                        } else {
+                            tagName += text[i];
+                            i++;
+                        }
+                    }
+                    tokens.push({ type: TokenType.CloseTag, value: tagName });
+                } else {
+                    let tagName = "";
+                    while (i < text.length) {
+                        if (text[i] == ' ') {
+                            break;
+                        } else {
+                            tagName += text[i];
+                            i++;
+                        }
+                    }
+                    isInTag = true;
+                    tokens.push({ type: TokenType.Tag, value: tagName });
+                }
+            } else if (text[i] == '>') {
+                isInTag = false;
+            } else if (text[i] == '?') {
+            } else if (text[i] != ' ') {
+                if (isInTag) {
+                    let attributeName = "";
+                    while (i < text.length) {
+                        if (text[i] == '=') {
+                            break;
+                        } else {
+                            attributeName += text[i];
+                            i++;
+                        }
+                    }
+                    tokens.push({ type: TokenType.AttributeName, value: attributeName });
+                    i++;
+                    let attributeValue = "";
+                    while (i < text.length) {
+                        if (text[i] == '"') {
+                            break;
+                        } else {
+                            i++;
+                        }
+                    }
+                    i++;
+                    while (i < text.length) {
+                        if (text[i] == '"') {
+                            break;
+                        } else {
+                            attributeValue += text[i];
+                            i++;
+                        }
+                    }
+
+                    tokens.push({ type: TokenType.AttributeValue, value: attributeValue });
+                } else {
+                    let content = "";
+                    while (i < text.length) {
+                        if (text[i] == '<') {
+                            break;
+                        } else {
+                            if (text[i] == ' ' || text[i] == '\n') {
+                                if (text[i - 1] != ' ') {
+                                    content += ' ';
+                                }
+                            } else {
+                                content += text[i];
+                            }
+                            i++;
+                        }
+                    }
+                    if (content != "" && content != " ") {
+                        tokens.push({ type: TokenType.Text, value: content });
+                    }
+                }
+            }
+            i++;
+        }
+        return tokens;
+    }
+}
