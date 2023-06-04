80510fa0d6c6b02f701dad824d598b2eb9fcfe27
rinrab <tima@chemodax.net>
2023-06-04 17:38:45 +0300

    Code refactor

--- xml.ts
+++ xml.ts
@@ -22,78 +22,48 @@ namespace XML {
                 if (text[i] == '/') {
                     i++;
                     let tagName = "";
-                    while (i < text.length) {
-                        if (text[i] == '>') {
-                            break;
-                        } else {
-                            tagName += text[i];
-                            i++;
-                        }
+                    while (i < text.length && text[i] != '>') {
+                        tagName += text[i++];
                     }
                     tokens.push({ type: TokenType.CloseTag, value: tagName });
                 } else {
                     let tagName = "";
-                    while (i < text.length) {
-                        if (text[i] == ' ') {
-                            break;
-                        } else {
-                            tagName += text[i];
-                            i++;
-                        }
+                    while (i < text.length && text[i] != ' ' && text[i] != '>') {
+                        tagName += text[i++];
                     }
                     isInTag = true;
                     tokens.push({ type: TokenType.Tag, value: tagName });
                 }
             } else if (text[i] == '>') {
                 isInTag = false;
-            } else if (text[i] == '?') {
-            } else if (text[i] != ' ') {
+            } else if (text[i] != ' ' && text[i] != '?') {
                 if (isInTag) {
                     let attributeName = "";
-                    while (i < text.length) {
-                        if (text[i] == '=') {
-                            break;
-                        } else {
-                            attributeName += text[i];
-                            i++;
-                        }
+                    while (i < text.length && text[i] != '=') {
+                        attributeName += text[i];
+                        i++;
                     }
                     tokens.push({ type: TokenType.AttributeName, value: attributeName });
                     i++;
                     let attributeValue = "";
-                    while (i < text.length) {
-                        if (text[i] == '"') {
-                            break;
-                        } else {
-                            i++;
-                        }
-                    }
+                    while (i < text.length && text[i] != '"') i++;
                     i++;
-                    while (i < text.length) {
-                        if (text[i] == '"') {
-                            break;
-                        } else {
-                            attributeValue += text[i];
-                            i++;
-                        }
+                    while (i < text.length && text[i] != '"') {
+                        attributeValue += text[i];
+                        i++;
                     }
-
                     tokens.push({ type: TokenType.AttributeValue, value: attributeValue });
                 } else {
                     let content = "";
-                    while (i < text.length) {
-                        if (text[i] == '<') {
-                            break;
-                        } else {
-                            if (text[i] == ' ' || text[i] == '\n') {
-                                if (text[i - 1] != ' ') {
-                                    content += ' ';
-                                }
-                            } else {
-                                content += text[i];
+                    while (i < text.length && text[i] != '<') {
+                        if (text[i] == ' ' || text[i] == '\n') {
+                            if (text[i - 1] != ' ') {
+                                content += ' ';
                             }
-                            i++;
+                        } else {
+                            content += text[i];
                         }
+                        i++;
                     }
                     if (content != "" && content != " ") {
                         tokens.push({ type: TokenType.Text, value: content });
